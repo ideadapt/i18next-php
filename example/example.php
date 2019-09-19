8812fd4a8264d9ffebc04f1e7d85811a3de190f1
ganti <ganti@users.noreply.github.com>
2019-09-19 20:47:05 +0200

    [update] example

--- example/example.php
+++ example/example.php
@@ -1,38 +1,33 @@
 <?php
-
-require_once('i18next.php');
+namespace Ganti;
+require_once('../src/i18next.php');
 
 try {
-
-	i18next::init('en');
-
+	i18next::init($language ='en', $path=null, $substitutionBracket="{{x}}");
 }
 catch (Exception $e) {
-
 	echo 'Caught exception: ' . $e->getMessage();
-
 }
 
 function t($key, $variables = array()) {
-
 	return i18next::getTranslation($key, $variables);
-
 }
 
-echo 'common.dog -> ' . t('common.dog');
+echo '<br>'.PHP_EOL.PHP_EOL. 	'common.dog -> ' . t('common.dog');
 
-echo '<br>';
+echo '<br>'.PHP_EOL.PHP_EOL. 	'common.cat { count: 0 } -> ' . t('common.cat', array('count' => 0));
+echo '<br>'.PHP_EOL.		 	'common.cat { count: 1 } -> ' . t('common.cat', array('count' => 1));
+echo '<br>'.PHP_EOL. 			'common.cat { count: 2 } -> ' . t('common.cat', array('count' => 2));
 
-echo 'common.cat { count: 1 } -> ' . t('common.cat', array('count' => 1));
+echo '<br>'.PHP_EOL.PHP_EOL. 	'common.elephant { count: 0 } -> ' . t('common.elephant', 0);
+echo '<br>'.PHP_EOL. 			'common.elephant { count: 1 } -> ' . t('common.elephant', 1);
+echo '<br>'.PHP_EOL. 			'common.elephant { count: 2 } -> ' . t('common.elephant', 2);
 
-echo '<br>';
 
-echo 'common.cat { count: 2 } -> ' . t('common.cat', array('count' => 2));
+echo '<br>'.PHP_EOL.PHP_EOL. 	'common.cat { count: 2, lng: fi } -> ' . t('common.cat', array('count' => 2, 'lng' => 'fi'));
 
-echo '<br>';
+echo '<br>'.PHP_EOL.PHP_EOL. 	'common.name_age { name: Elisa, age: 32 } -> ' . t('common.name_age', array('name' => "Elisa", "age" => 32));
 
-echo 'common.cat { count: 2, lng: fi } -> ' . t('common.cat', array('count' => 2, 'lng' => 'fi'));
-
-echo '<hr>Array: common.thedoglovers -><br>';
-
-echo str_replace("\n", '<br>', t('common.thedoglovers'));
+echo PHP_EOL.PHP_EOL.			'<hr>Array: common.thedoglovers -><br>'.PHP_EOL;
+echo 							str_replace("\n", '<br>', t('common.thedoglovers', array('animal' => 'cat')));
+echo '<br>'.PHP_EOL.PHP_EOL;
